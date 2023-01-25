f19a5452588c76ea15844fbaee01567d6306911a
mihsamusev <mih.samusev@gmail.com>
2023-01-25 17:58:58 +0100

    dependencies + secret generating script

--- cli/cli.py
+++ cli/cli.py
@@ -4,10 +4,12 @@ import requests
 ENDPOINT = "http://localhost:8888"
 app = typer.Typer()
 
+
 @app.command()
 def hello():
     response = requests.get(url=ENDPOINT)
     print(response.json())
+
 
 if __name__ == "__main__":
     app()
--- cli/server.py
+++ cli/server.py
@@ -1,28 +1,42 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic import BaseModel
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ASSESS_JWT_EXPIRE_MINUTES = os.getenv("ACCESS_JWT_EXPIRE_MINUTES")
 
 class User(BaseModel):
     username: str
 
+
 class UserDB(User):
     hashed_password: str
 
+class Token(BaseModel):
+    assess_token: str
+    token_type: str
+
+class Tokendata(BaseModel):
+    username: str
+
+
 fake_users_db = {
-    "alice": {
-        "username": "alice",
-        "hashed_password": "fakehashedsecret"
-    },
-    "bob": {
-        "username": "bob",
-        "hashed_password": "fakehashedsecret2"
-    }
+    "alice": {"username": "alice", "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"},
 }
+
 
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserDB(**user_dict)
+
 
 def hash_password(password: str):
     return "fakehashed" + password
@@ -33,7 +47,15 @@ def fake_decode_token(token):
 
 
 app = FastAPI()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     """
@@ -44,9 +66,10 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentification credentials",
-            headers={"WWW-Authenticate": "Bearer"}
+            headers={"WWW-Authenticate": "Bearer"},
         )
     return user
+
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
