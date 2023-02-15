3526dc91408aef68b58f497adb4df979b78db5d9
mihsamusev <mhs@bluepp.dk>
2023-02-15 16:35:18 +0100

    pytest works but its wrong!

--- tests/decorators/test_decorators_add_class_methods.py
+++ tests/decorators/test_decorators_add_class_methods.py
@@ -1,24 +1,58 @@
+"""
+By default, fixtures have the function scope.
+This means that a new instance of the fixture function is created
+for each test function that uses it.
+"""
+
 from tinypybits.decorators import ElectrolyzerSunfire, ElectrolyzerTK
 import pytest
 
 
 @pytest.fixture()
-def unique_electrolyzers():
+def distinct_electrolyzers():
     return [ElectrolyzerSunfire(), ElectrolyzerTK()]
+
 
 @pytest.fixture()
 def same_electrolyzers():
     return [ElectrolyzerSunfire(), ElectrolyzerSunfire()]
 
+
 def test_add_variables_decorator_creates_methods_for_the_eletrolyzer_classes(
-    unique_electrolyzers,
+    distinct_electrolyzers,
 ):
-    for electrolyzer in unique_electrolyzers:
+    for electrolyzer in distinct_electrolyzers:
         electrolyzer.power_input(t=1)
         electrolyzer.h2_output(t=1)
 
 
-def test_different_classes_with_same_variables_attached_point_to_different_method_ids(
-    unique_electrolyzers,
+def test_instances_of_distinct_type_receive_unique_method_ids_on_decorator_application(
+    distinct_electrolyzers,
 ):
-    assert id(unique_electrolyzers[0].power_input) != id(unique_electrolyzers[1].power_input)
+    assert id(distinct_electrolyzers[0].power_input) != id(
+        distinct_electrolyzers[1].power_input
+    )
+    assert id(distinct_electrolyzers[0].h2_output) != id(
+        distinct_electrolyzers[1].h2_output
+    )
+
+
+def test_instances_of_same_type_receive_unique_method_ids_on_decorator_application(
+    same_electrolyzers,
+):
+    assert id(same_electrolyzers[0].power_input) != id(
+        same_electrolyzers[1].power_input
+    )
+    assert id(same_electrolyzers[0].h2_output) != id(same_electrolyzers[1].h2_output)
+
+
+def test_instances_receive_unique_method_ids_on_decorator_application_no_fixtures():
+    sunfire = ElectrolyzerSunfire()
+    tk_first = ElectrolyzerTK()
+    tk_second = ElectrolyzerTK()
+    assert id(sunfire.power_input) != id(tk_first.power_input)
+    assert id(tk_first.power_input) != id(tk_second.power_input)
+
+
+if __name__ == "__main__":
+    test_instances_receive_unique_method_ids_on_decorator_application_no_fixtures()
--- tinypybits/decorators/decorators_inits.py
+++ tinypybits/decorators/decorators_inits.py
@@ -3,6 +3,11 @@ import inspect
 from copy import deepcopy
 
 
+def record_call():
+    with open("how_many_imports.log", "a") as fout:
+        fout.write("im imported again!\n")
+
+
 class Variable:
     pass
 
@@ -14,17 +19,25 @@ class ModelObject:
 
 class PowerInput(Variable):
     def power_input(self, t: int):
-        print(f"im power input at time t={t}")
+        print(
+            f"im power input at time t={t} of object {self.__class__.__name__}:{id(self)}"
+        )
 
 
 class H2Output(Variable):
     def h2_output(self, t: int):
-        print(f"im h2 output at time t={t}")
+        print(
+            f"im h2 output at time t={t} of object {self.__class__.__name__}:{id(self)}"
+        )
 
 
-def add_mixins(variable_types: List[Variable]):
+def add_mixins(variables: List[Variable]):
+    """
+    Generates new type with target class methods and methods from given variable classes
+    """
+
     def inner(target_class: ModelObject):
-        inherited_types = tuple([target_class]) + tuple(variable_types)
+        inherited_types = tuple([target_class]) + tuple(variables)
         fused_type = type(target_class.__class__.__name__, inherited_types, {})
         return fused_type
 
@@ -32,13 +45,15 @@ def add_mixins(variable_types: List[Variable]):
 
 
 def add_variables(variables: List[Variable]):
+    """
+    Reuses target class but sets new methods from given variable classes
+    """
+
     def inner(target_class: ModelObject):
         for variable in variables:
-            public_functions = inspect.getmembers(
-                variable, predicate=inspect.isfunction
-            )
-            for name, callable in public_functions:
-                setattr(target_class, name, callable)
+            public_methods = inspect.getmembers(variable, predicate=inspect.isfunction)
+            for method_name, method_callable in public_methods:
+                setattr(target_class, method_name, method_callable)
         return target_class
 
     return inner
@@ -56,7 +71,7 @@ class ElectrolyzerSunfire(ModelObject):
         print("Im sunfire")
 
 
-if __name__ == "__main__":
+def example():
     electrolyzer = ElectrolyzerTK()
     print(electrolyzer.__class__, electrolyzer.__class__.__bases__)
     electrolyzer.power_input(t=1)
@@ -67,4 +82,13 @@ if __name__ == "__main__":
     sunfire.power_input(t=1)
     sunfire.h2_output(t=1)
 
+    another_sunfire = ElectrolyzerSunfire()
+
+    print(id(electrolyzer.power_input), id(sunfire.power_input))
     assert id(electrolyzer.power_input) != id(sunfire.power_input)
+    assert id(electrolyzer.h2_output) != id(sunfire.h2_output)
+    assert id(sunfire.h2_output) != id(another_sunfire.h2_output)
+
+
+if __name__ == "__main__":
+    example()
